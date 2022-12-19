61d847d8ebf0b7845c71fe70a93e1ef965d0b5ed
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2022-12-20 00:40:30 +0300

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/murky/clbva7cml000415ldum09l5lf",
+  center: [38.05447, 0.2047],
+  zoom: 5.8,
+});
