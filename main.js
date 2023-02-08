c98eb15e46ed863aac3cfd9a21ec7b9273693662
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2023-02-08 11:37:44 +0300

    update

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
 var map = new mapboxgl.Map({
-  container: "map",
+  container: "blank_map",
   style: "mapbox://styles/murky/clbva7cml000415ldum09l5lf",
   center: [38.05447, 0.2047],
   zoom: 5.8,
+  minZoom: 5.8,
+  maxZoom: 7.5,
 });
 
-map.on("load", function () {
-  map.addSource("mobilemoney", {
-    type: "geojson",
-    data: "data/agents.geojson",
-  });
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
+var map = new mapboxgl.Map({
+  container: "market_map",
+  style: "mapbox://styles/murky/cldvd4r4e002t01plmwgdxzgs",
+  center: [38.05447, 0.2047],
+  zoom: 5.8,
+  minZoom: 5.8,
+  maxZoom: 7.5,
 });
 
-map.on("load", function () {
-  map.addLayer({
-    id: "mobilemoney",
-    type: "symbol",
-    source: "mobilemoney",
-    layout: {
-      "icon-image": "{icon}-15",
-      "icon-allow-overlap": true,
-    },
-  });
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
+var map = new mapboxgl.Map({
+  container: "fsp_map",
+  style: "mapbox://styles/murky/cldu916gi000z01s041jsga9w",
+  center: [38.05447, 0.2047],
+  zoom: 5.8,
+  minZoom: 5.8,
+  maxZoom: 7.5,
+});
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
+var map = new mapboxgl.Map({
+  container: "primary_school_map",
+  style: "mapbox://styles/murky/cldvbixks001201ru2ymn3syz",
+  center: [38.05447, 0.2047],
+  zoom: 5.8,
+  minZoom: 5.8,
+  maxZoom: 7.5,
 });
