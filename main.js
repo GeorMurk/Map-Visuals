77f902d834e698a5774b28d68193ea987b76defc
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2023-02-10 17:06:41 +0300

    update

--- main.js
+++ main.js
@@ -1,43 +1,78 @@
 mapboxgl.accessToken =
-  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
+  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2xkeTRhZDZ5MDh4MDNxbzd1Zjk2eWN1aCJ9.RT5-9Y9H_u16JWuXouE5vg";
 var map = new mapboxgl.Map({
   container: "blank_map",
-  style: "mapbox://styles/murky/clbva7cml000415ldum09l5lf",
+  style: "mapbox://styles/murky/clbv454x9006s14o9jfiwup1k",
   center: [38.05447, 0.2047],
   zoom: 5.8,
   minZoom: 5.8,
-  maxZoom: 7.5,
+  // maxZoom: 7.3,
 });
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
-var map = new mapboxgl.Map({
-  container: "market_map",
-  style: "mapbox://styles/murky/cldvd4r4e002t01plmwgdxzgs",
-  center: [38.05447, 0.2047],
-  zoom: 5.8,
-  minZoom: 5.8,
-  maxZoom: 7.5,
+// Schools
+
+map.on("load", () => {
+  map.addSource("schools", {
+    type: "geojson",
+    data: "data/schools.geojson",
+  });
+
+  map.addLayer({
+    id: "schools-layer",
+    type: "circle",
+    source: "schools",
+    // layout: { visibility: "visible" },
+    paint: {
+      "circle-radius": 4,
+      "circle-stroke-width": 0.5,
+      "circle-color": "brown",
+      "circle-stroke-color": "white",
+    },
+    // "source-layer": "schools-cusco",
+  });
 });
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
-var map = new mapboxgl.Map({
-  container: "fsp_map",
-  style: "mapbox://styles/murky/cldu916gi000z01s041jsga9w",
-  center: [38.05447, 0.2047],
-  zoom: 5.8,
-  minZoom: 5.8,
-  maxZoom: 7.5,
+// FSP Agents
+
+map.on("load", () => {
+  map.addSource("agents", {
+    type: "geojson",
+    data: "data/agents.geojson",
+  });
+
+  map.addLayer({
+    id: "agents-layer",
+    type: "circle",
+    source: "agents",
+    // layout: { visibility: "visible" },
+    paint: {
+      "circle-radius": 4,
+      "circle-stroke-width": 0.5,
+      "circle-color": "green",
+      "circle-stroke-color": "white",
+    },
+    // "source-layer": "agents-cusco",
+  });
 });
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibXVya3kiLCJhIjoiY2tvMTdsODM3MDV3dDMyb2VrNGJzMTl4dCJ9.-iaLxmNqNcVaQXmtnxpP3w";
-var map = new mapboxgl.Map({
-  container: "primary_school_map",
-  style: "mapbox://styles/murky/cldvbixks001201ru2ymn3syz",
-  center: [38.05447, 0.2047],
-  zoom: 5.8,
-  minZoom: 5.8,
-  maxZoom: 7.5,
+// Markets
+
+map.on("load", () => {
+  map.addSource("markets", {
+    type: "geojson",
+    data: "data/KAMIS_MAP_1/Other_Markets.geojson",
+  });
+
+  map.addLayer({
+    id: "markets-layer",
+    type: "circle",
+    source: "markets",
+    // layout: { visibility: "visible" },
+    paint: {
+      "circle-radius": 4,
+      "circle-stroke-width": 0.5,
+      "circle-color": "blue",
+      "circle-stroke-color": "white",
+    },
+  });
 });
